a34d930ca6fa1f03251055f4d0f22c597173a234
TheSawIsTheLaw <dimakrok@bk.ru>
2021-07-12 17:42:57 +0300

    SHT: we need a time to get some info about PyQT...

---
+++ app/autoDoc/configurator.py
@@ -0,0 +1,8 @@
+class Config:
+    newFileName = "newReport"  # Check if library will make .docx by itself or not
+    pathForNewFile = ""
+    pathToTable = ""
+
+
+    def fullConfigByNewOne(self, path):
+        pass
