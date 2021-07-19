69038f8a95b6f2920b0b73c238c3894580ea08e5
TheSawIsTheLaw <dimakrok@bk.ru>
2021-07-19 16:06:48 +0300

    ADD: functions for config dictionary

--- app/src/python/test.py
+++ app/src/python/test.py
@@ -4,23 +4,60 @@ from openpyxl import load_workbook
 import codecs
 
 
+# https://stackoverflow.com/questions/55115070/next-letter-in-alphabet
+def nextAlphabetLetter(s):
+    return chr((ord(s.upper()) + 1 - 65) % 26 + 65)
+
+
+def throwWrongStructure():
+    raise Exception("Конфигурационный файл не соответствует требуемому формату")
+
+
+# fTest: python test.py a a C:\Users\dobri\Desktop\ReportAutomation\app\src\config\stdConfig.ycfg a
+def goToNextSharps(lines, curLineNumber):
+    while curLineNumber < len(lines):
+        if lines[curLineNumber][:3] == "###":
+            return curLineNumber
+        curLineNumber += 1
+    return -1
+
+
 def getRulesFromConfig(configPath):
     outRules = dict()
 
     with open(configPath, encoding = 'utf-8') as file:
         lines = file.readlines()
 
-    for curLine in lines:
-        threeFirstChars = curLine[:-3]
-        if threeFirstChars == "!--":
-            continue
-        # Тут потребуется декомпозировать на отдельные функции. При нахождении определённого
-        # раздела конфига - ищем конец раздела и передаём строки в функцию на проверку
-        elif threeFirstChars ==
+    curLineNumber = goToNextSharps(lines, 0)
+    # print(lines[curLineNumber].replace(" \n\0", ""))
+    if lines[curLineNumber] != "### Область названий организаций\n":
+        throwWrongStructure()
 
-    print(lines)
+    curLineNumber += 1
+    outRules["startOfTable"] = lines[curLineNumber].split(" ")
+    outRules["startOfTable"] = map(lambda curNum: int(curNum), outRules["startOfTable"])
+
+    #    for i in outRules["startOfTable"]:
+    #        print("!!{}".format(i))
+
+    curLineNumber = goToNextSharps(lines, curLineNumber)
+    curLineNumber += 1
+    if curLineNumber == len(lines):
+        throwWrongStructure()
+
+    headersInDocument = []
+    curHeaderDescription = lines[curLineNumber]
+    while curLineNumber < len(lines) - 1 and curHeaderDescription[0] == "[":
+        curHeaderDescription = curHeaderDescription.replace("\n", "").split(" ")
+        headersInDocument.append(curHeaderDescription)
+
+        curLineNumber += 1
+        curHeaderDescription = lines[curLineNumber]
+
+    outRules["headers"] = headersInDocument
 
     return outRules
+
 
 '''
     REALLY FUCKING IMPORTANT!!!
@@ -30,17 +67,21 @@ def getRulesFromConfig(configPath):
     
     I think you've got it, right?
 '''
-### REALLY IMPORTANT:
+
+
 def main():
+    print()
     gotArgs = sys.argv
 
-    tablePath = sys.argv[1]         # path to table to get info from
-    docSavePath = sys.argv[2]       # path to save document
-    configPath = sys.argv[3]        # path to configuration
-    workRowNumber = sys.argv[4]     # number of row from table
+    tablePath = sys.argv[1]  # path to table to get info from
+    docSavePath = sys.argv[2]  # path to save document
+    configPath = sys.argv[3]  # path to configuration
+    workRowNumber = sys.argv[4]  # number of row from table
 
     dictOfRulesForDoc = getRulesFromConfig(configPath)
+    print(dictOfRulesForDoc["headers"][0][1])
 
     return 0
+
 
 main()
