fe301fff43aecc183c492ef3a8bd84f436cfc0e5
TheSawIsTheLaw <dimakrok@bk.ru>
2021-07-27 13:04:09 +0300

    ADD: doxygen comments

--- app/src/autoDoc/mainwindow.cpp
+++ app/src/autoDoc/mainwindow.cpp
@@ -3,10 +3,19 @@
 #include "ui_mainwindow.h"
 
 #include "reportcreator.hpp"
+
+/**
+ * \fn MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
+ * \brief MainWindow init function
+ *
+ * \param parent a parent O.o
+ *
+ * \return MainWindow class with facade inited
+ */
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    this->setWindowTitle(QString("Тук-тук, кто там? Плохой нейминг"));
+    this->setWindowTitle(QString("Отчёто-генератор-2000"));
     // Without BBB sound today :(
 
     facade = new Facade();
@@ -14,8 +23,22 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
 
 MainWindow::~MainWindow() { delete ui; }
 
+/**
+ * \fn void MainWindow::on_MakeReportButton_clicked()
+ * \brief Slot for MakeReportButton
+ *
+ *  Inits choice of organisations from facade.
+ *  Opens new window.
+ */
 void MainWindow::on_MakeReportButton_clicked() { facade->initChoiceOfOrgs(this); }
 
+/**
+ * \fn void MainWindow::on_SetSavePathButton_clicked()
+ * \brief Slot for set SetSavePathButton
+ *
+ *  Gets directory to save generated document.
+ *  Opens QFileDialog.
+ */
 void MainWindow::on_SetSavePathButton_clicked()
 {
     QString gotPath =
@@ -27,6 +50,13 @@ void MainWindow::on_SetSavePathButton_clicked()
     }
 }
 
+/**
+ * \fn void MainWindow::on_TableFileButton_clicked()
+ * \brief Slot for set TableFileButton
+ *
+ *  Gets directory of excel document that contains all info.
+ *  Opens QFileDialog.
+ */
 void MainWindow::on_TableFileButton_clicked()
 {
     QString gotFilePath =
@@ -38,6 +68,13 @@ void MainWindow::on_TableFileButton_clicked()
     }
 }
 
+/**
+ * \fn void MainWindow::on_LoadNewConfigButton_clicked()
+ * \brief Slot for set LoadNewConfigButton
+ *
+ *  Gets path of new configuration file and set it as used.
+ *  Opens QFileDialog and sets config using facade.
+ */
 void MainWindow::on_LoadNewConfigButton_clicked()
 {
     QString gotFilePath = QFileDialog::getOpenFileName(
