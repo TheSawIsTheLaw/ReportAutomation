81916f1f3e413de3077f83ca5f4c3f0e153d6b63
TheSawIsTheLaw <dimakrok@bk.ru>
2021-07-27 13:35:59 +0300

    FIX: style of constructor ADD: doxygen comments

--- app/src/autoDoc/autodocconfiguration.cpp
+++ app/src/autoDoc/autodocconfiguration.cpp
@@ -2,11 +2,26 @@
 
 AutoDocConfiguration::AutoDocConfiguration() {}
 
+/**
+ * \fn AutoDocConfiguration::AutoDocConfiguration(QString savePath_, QString
+ * readFilePath_, QString startCell_, QString configPath_) : savePath(savePath_),
+ * readFilePath(readFilePath_), startCell(startCell_), configPath(configPath_)
+ * \brief Init of AutoDocConfiguration class
+ *
+ * \param savePath_ - path to save generated file
+ * \param readFilePath_ - path of read file (excel table in our case)
+ * \param startCell_ - coordinates in excel style of start cell
+ * \param configFile_ - path to configuration file
+ *
+ *  I guess, i should not use a class but...
+ *
+ *  \return Inited ExcelWorker class
+ */
 AutoDocConfiguration::AutoDocConfiguration(
     QString savePath_, QString readFilePath_, QString startCell_, QString configPath_)
+: savePath(savePath_)
+, readFilePath(readFilePath_)
+, startCell(startCell_)
+, configPath(configPath_)
 {
-    savePath = savePath_;
-    readFilePath = readFilePath_;
-    startCell = startCell_;
-    configPath = configPath_;
 }
