e4241c3760984bf34da6abb0231b22d470e7fa38
TheSawIsTheLaw <dimakrok@bk.ru>
2021-07-27 13:56:48 +0300

    FIX: style

--- app/src/autoDoc/reportcreator.cpp
+++ app/src/autoDoc/reportcreator.cpp
@@ -2,27 +2,46 @@
 #include <QDebug>
 #include <qtextcodec.h>
 
+/**
+ * \fn ReportCreator::ReportCreator(
+ * QString analyzeFile_, QString savePath_, QString configPath_, int row_)
+ * \brief Init of ReportCreator class
+ *
+ *  Inits ReportCreator using passed values
+ *
+ *  \return Inited ReportCreator class
+ */
 ReportCreator::ReportCreator(
     QString analyzeFile_, QString savePath_, QString configPath_, int row_)
+: analyzeFile(analyzeFile_), savePath(savePath_), configPath(configPath_), row(row_)
 {
-    analyzeFile = analyzeFile_;
-    savePath = savePath_;
-    configPath = configPath_;
-    row = row_;
 }
 
+/**
+ * \fn int ReportCreator::startProc()
+ * \brief Starts python script to generate one report
+ *
+ *  Prepares arguments and sets a program to a QProcess.
+ *  Then executes it.
+ *
+ *  \return Exit code of executed script
+ */
 int ReportCreator::startProc()
 {
+    QStringList arguments = QStringList() << analyzeFile.toStdString().c_str() << savePath
+                                          << configPath << QString::number(row);
+    //    qDebug() << gavno;
+
     QProcess *creatorProc = new QProcess();
-    QTextCodec::setCodecForLocale(QTextCodec::codecForLocale());
-    QStringList gavno = QStringList() << analyzeFile.toStdString().c_str() << savePath << configPath << QString::number(row);
-    qDebug() << gavno;
-    creatorProc->setArguments(gavno);
-    creatorProc->setProgram("C:/Users/dobri/Desktop/ReportAutomation/app/src/python/dist/reportGenerator.exe");
+    creatorProc->setArguments(arguments);
+    creatorProc->setProgram("reportGenerator.exe");
+
     creatorProc->start();
     creatorProc->waitForFinished();
-//    qDebug() << creatorProc->readAllStandardError().data();
-//    qDebug() << QTextCodec::codecForName("cp1251")->toUnicode(creatorProc->readAll().data());
-//    qDebug() << creatorProc->exitCode();
+
+    //    qDebug() << creatorProc->readAllStandardError().data();
+    //    qDebug() <<
+    //    QTextCodec::codecForName("cp1251")->toUnicode(creatorProc->readAll().data());
+    //    qDebug() << creatorProc->exitCode();
     return creatorProc->exitCode();
 }
