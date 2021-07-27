7a4a69b7fb7cca87be98c07d595462c13cf29a65
TheSawIsTheLaw <dimakrok@bk.ru>
2021-07-27 13:15:09 +0300

    ADD: doxygen comments DEL: unused method

--- app/src/autoDoc/facade.cpp
+++ app/src/autoDoc/facade.cpp
@@ -1,13 +1,30 @@
 #include "facade.hpp"
 
-Facade::Facade()
+/**
+ * \fn Facade::Facade() : configuration(new AutoDocConfiguration())
+ * \brief Facade init function
+ *
+ * \return Facade class with configuration inited
+ */
+Facade::Facade() : configuration(new AutoDocConfiguration())
 {
-    configuration = new AutoDocConfiguration();
-    setConfigByFile(PATH_TO_CONF); // !!!!!!!!!!!!!!!!!!!!!! Потребуется изменить
+    setConfigByFile(PATH_TO_CONF);
 }
 
+/**
+ * \fn void Facade::setSavePathForConfiguration(QString path)
+ * \brief Save path of configuration file
+ *
+ * \param QString path - path of new configuration file
+ */
 void Facade::setSavePathForConfiguration(QString path) { configuration->savePath = path; }
 
+/**
+ * \fn void Facade::setAnalyzeFile(QString filePath)
+ * \brief Save path to analyze file (excel table in our case)
+ *
+ * \param QString filePath - path to excel table
+ */
 void Facade::setAnalyzeFile(QString filePath)
 {
     if (std::filesystem::exists(filePath.toUtf8().toStdString()))
@@ -17,6 +34,15 @@ void Facade::setAnalyzeFile(QString filePath)
     //    qDebug("Filepath: '%s'", filePath.toStdString().c_str());
 }
 
+/**
+ * \fn int Facade::setConfigByFile(QString filePath)
+ * \brief Save path to configuration file and sets value of
+ * startCell field in AutoDocConfiguration class
+ *
+ * \param Qstring filePath - path to configuration file
+ *
+ * \return 0 if success
+ */
 int Facade::setConfigByFile(QString filePath)
 {
     if (!std::filesystem::exists(filePath.toStdString()))
@@ -35,6 +61,13 @@ int Facade::setConfigByFile(QString filePath)
     return 0;
 }
 
+/**
+ * \fn void Facade::initChoiceOfOrgs(QWidget *parent)
+ * \brief Opens new window to get user's choice of needed
+ * orgs and to generate a report(s)
+ *
+ * \param QWidget *parent - a pointer to the parent O.o
+ */
 void Facade::initChoiceOfOrgs(QWidget *parent)
 {
     if (configuration->readFilePath == "" || configuration->startCell == "" ||
@@ -46,6 +79,7 @@ void Facade::initChoiceOfOrgs(QWidget *parent)
         QErrorMessage *errorWin = new QErrorMessage();
         errorWin->setWindowTitle("Ошибка");
         errorWin->showMessage("Не заданы параметры для выполнения работы.");
+
         return;
     }
 
@@ -53,11 +87,4 @@ void Facade::initChoiceOfOrgs(QWidget *parent)
 
     orgChoiceWin.setModal(true);
     orgChoiceWin.exec();
-}
-
-int Facade::generateReport(int row)
-{
-    ReportCreator creator(configuration->readFilePath, configuration->savePath,
-        configuration->configPath, row);
-    return creator.startProc();
 }
