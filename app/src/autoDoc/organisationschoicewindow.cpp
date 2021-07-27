2b2326c42620b13480003f2d002cf4530d5c9bdc
TheSawIsTheLaw <dimakrok@bk.ru>
2021-07-27 13:20:10 +0300

    ADD: doxygen comments

--- app/src/autoDoc/organisationschoicewindow.cpp
+++ app/src/autoDoc/organisationschoicewindow.cpp
@@ -2,18 +2,18 @@
 #include "ui_organisationschoicewindow.h"
 
 OrganisationsChoiceWindow::OrganisationsChoiceWindow(
-    AutoDocConfiguration &conf, QWidget *parent)
-: QDialog(parent), ui(new Ui::OrganisationsChoiceWindow)
+    AutoDocConfiguration &configuration_, QWidget *parent)
+: QDialog(parent), ui(new Ui::OrganisationsChoiceWindow), configuration(configuration_)
 {
     ui->setupUi(this);
     this->setWindowTitle("Обнаруженные данные в таблице");
-    configuration = conf;
 
     ui->listOfFoundedCompanies->setSelectionMode(QAbstractItemView::MultiSelection);
 
     ExcelWorker excelReader;
-    std::vector<QString> names =
-        excelReader.getFirstCellsText(conf.readFilePath, configuration.startCell);
+    std::vector<QString> names = excelReader.getFirstCellsText(
+        configuration.readFilePath, configuration.startCell);
+
     for (std::vector<QString>::iterator it = names.begin(); it != names.end(); it++)
         ui->listOfFoundedCompanies->addItem((*it).remove('\n'));
 }
@@ -22,37 +22,43 @@ OrganisationsChoiceWindow::~OrganisationsChoiceWindow() { delete ui; }
 
 void OrganisationsChoiceWindow::on_buttonsGroup_accepted()
 {
-    QList<QModelIndex> items = ui->listOfFoundedCompanies->selectionModel()->selectedIndexes();
+    QList<QModelIndex> items =
+        ui->listOfFoundedCompanies->selectionModel()->selectedIndexes();
 
     int exitCode = 0;
 
     QProgressDialog progressDial("Генерим отчёты...", "Отмена", 0, items.size(), nullptr);
     progressDial.setModal(true);
-    progressDial.setValue(0);
-    progressDial.wasCanceled();
+
     for (int i = 0; i < items.size() && !exitCode && !progressDial.wasCanceled(); i++)
     {
         qApp->processEvents();
         progressDial.setValue(i);
+
         exitCode = ReportCreator(configuration.readFilePath, configuration.savePath,
-                                 configuration.configPath, items.at(i).row())
+            configuration.configPath, items.at(i).row())
                        .startProc();
+
         if (exitCode == 1)
         {
             QErrorMessage *err = new QErrorMessage(this);
             err->setWindowTitle("Ошибка");
-            err->showMessage("Произошла ошибка во время обработки таблицы (" + ui->listOfFoundedCompanies->selectedItems().at(i)->text() + ").\nПроверьте "
+            err->showMessage("Произошла ошибка во время обработки таблицы (" +
+                             ui->listOfFoundedCompanies->selectedItems().at(i)->text() +
+                             ").\nПроверьте "
                              "целостность конфигурационного файла или таблицы.");
         }
         else if (exitCode == 2)
         {
             QErrorMessage *err = new QErrorMessage(this);
             err->setWindowFilePath("Ошибка");
-            err->showMessage("Файл не доступен для записи (" + ui->listOfFoundedCompanies->selectedItems().at(i)->text() + "). Закройте документ и повторите "
+            err->showMessage("Файл не доступен для записи (" +
+                             ui->listOfFoundedCompanies->selectedItems().at(i)->text() +
+                             "). Закройте документ и повторите "
                              "попытку ещё раз.");
         }
-
     }
+
     progressDial.setValue(items.size());
     progressDial.close();
 }
