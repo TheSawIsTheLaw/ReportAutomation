cb40c7ef20ea7adbe9f9c313fe59cb05057c2236
Yakuba Dmitry <55848738+TheSawIsTheLaw@users.noreply.github.com>
2021-08-04 00:15:52 +0300

    DEL: python qt pre-version

--- app/src/depr/configurator.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    newFileName = "newReport"  # Check if library will make .docx by itself or not
-    pathForNewFile = ""
-    pathToTable = ""
-
-
-    def fullConfigByNewOne(self, path):
-        pass
